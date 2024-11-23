8de3c5e0ecffcbc2a0a32a9594b33158cc043ea7
ddewaele <ddewaele@gmail.com>
2024-11-23 20:57:14 +0100

    initial commit

--- lib/cdk-simple-dynamodb-stack.ts
+++ lib/cdk-simple-dynamodb-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkSimpleDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a DynamoDB table
+    const table = new Table(this, 'MoviesTable', {
+      tableName: 'MoviesTable',
+      partitionKey: { name: 'PK', type: AttributeType.STRING },
+      sortKey: { name: 'SK', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSimpleDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Add a Global Secondary Index (GSI) for querying by year
+    table.addGlobalSecondaryIndex({
+      indexName: 'YearIndex',
+      partitionKey: { name: 'year', type: AttributeType.NUMBER },
+      projectionType: cdk.aws_dynamodb.ProjectionType.ALL, // Include all attributes in the index
+    });
   }
 }
